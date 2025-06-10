1d30c3d10909357ae16559ba7d48bb883a41533d
ErenSakarya7 <115359053+ErenSakarya7@users.noreply.github.com>
2025-06-10 09:08:52 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-curl -s "https://script.google.com/macros/s/AKfycbxY63yN2iA8vEKj1zM5Ozr_LJ60b-SboWoKRZHLnVPyxqSm5twQYUupqsOqBswmPo9UCw/exec" -o adminlist.cfg
+curl -sL "https://script.google.com/macros/s/AKfycbxY63yN2iA8vEKj1zM5Ozr_LJ60b-SboWoKRZHLnVPyxqSm5twQYUupqsOqBswmPo9UCw/exec" -o adminlist.cfg
